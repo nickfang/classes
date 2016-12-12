6da058a356f444b6dc20cddbf7149855cd6ceffc
nfang <fang.nicholas@gmail.com>
2016-12-12 10:03:34 -0600

    Noticed that this file from projectEuler wasn't committed.

---
+++ projectEuler/getTemplate.py
@@ -0,0 +1,12 @@
+import os
+from shutil import copyfile
+
+def getTemplate(problemNumber, src="webScraping\\problemTemplates", dest="python"):
+	templateFile = os.path.join(src, problemNumber)
+	if os.path.isFile(templateFile):
+		copyfile(os.path.join(src, problemNumber), os.path.join(dest, problemNumber))
+	else:
+		print("Template file " + templateFile + " does not exist.")
+
+
+if __name__ == "__main__":
