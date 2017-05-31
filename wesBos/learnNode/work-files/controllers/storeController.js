771a18c5799b3d334f601185078d466c0af0107b
nfang <fang.nicholas@gmail.com>
2017-05-31 14:20:37 -0500

    Got through implementing flash.  Also added the edit routes.

--- wesBos/learnNode/work-files/controllers/storeController.js
+++ wesBos/learnNode/work-files/controllers/storeController.js
@@ -14,7 +14,7 @@ exports.addStore = (req, res) => {
 exports.createStore = async (req, res) => {
 	// NOTE: check if the data being passed back is good.
 	// res.json(req.body);
-	const store = new Store(req.body);
+	const store = await (new Store(req.body)).save();
 	// NOTE: how to add new elemnts to mongodb
 	// store.age = 10
 	// store.location = "corner of elm and late"
@@ -23,6 +23,33 @@ exports.createStore = async (req, res) => {
 	// NOTE: await will not move on until the save has happened
 	// NOTE: to catch an error with async await, you have to wrap your code with a try...  or use composition.  wrap createStore that handles the error.  catchErrors in errorHandlers handles this.
 	// NOTE: app.js will pass from "/" to the error handlers and stop on the one that works
-	await store.save();
-	res.redirect("/");
+	req.flash("success", `Successfully Created ${store.name}.  Care to leave a review?`);
+	res.redirect(`/store/${store.slug}`);
+
+}
+
+exports.getStores = async (req, res) => {
+	const stores = await Store.find();
+	console.log(stores);
+	res.render("stores", {title: 'Stores', stores});
+}
+
+exports.editStore = async (req, res) => {
+	// find the store given the ID
+	const store = await Store.findOne({ _id: req.params.id });
+	// TODO: confirm they are the owner of the store
+	// Render out the edit form so the user can update
+	// NOTE: in ES6, if the key and value are the same, you just have to write it once.
+	res.render("editStore", { title: `Edit ${store.name}`, store});
+}
+
+exports.updateStore = async(req, res) => {
+	// find and update the store
+	const store = await Store.findOneAndUpdate({ _id: req.params.id}, req.body, {
+		new: true, // return the new store instead of the old one
+		runValidators: true
+	}).exec();
+	req.flash("success", `Successfully updated <strong>${store.name}</strong>. <a href="/store/${store.slug}">View Store </a>`);
+	res.redirect(`/stores/${store._id}/edit`);
+	// redirect them to the store and tell them it worked
 }
--- wesBos/learnNode/work-files/routes/index.js
+++ wesBos/learnNode/work-files/routes/index.js
@@ -4,9 +4,12 @@ const storeController = require('../controllers/storeController');
 
 const {catchErrors} = require("../handlers/errorHandlers");
 
-router.get('/', storeController.homePage);
+router.get('/', catchErrors(storeController.getStores));
+router.get('/stores', catchErrors(storeController.getStores));
 router.get("/add", storeController.addStore);
 // NOTE: passing storeController.createStore into catchError and passes back a catch
 router.post("/add", catchErrors(storeController.createStore));
+router.get("/stores/:id/edit", catchErrors(storeController.editStore));
+router.post("/add/:id", catchErrors(storeController.updateStore));
 
 module.exports = router;
