968031b57362c5707f467030dcbde18ac1dfef10
nfang <fang.nicholas@gmail.com>
2018-01-23 01:22:41 -0600

    Saving what I did today.

--- udemy/machineLearningA-Z/Part 1 - Data Preprocessing/Data_Preprocessing/data_preprocessing_template.R
+++ udemy/machineLearningA-Z/Part 1 - Data Preprocessing/Data_Preprocessing/data_preprocessing_template.R
@@ -2,6 +2,10 @@
 
 # Importing the dataset
 dataset = read.csv('Data.csv')
+dataset$Age = ifelse(is.na(dataset$Age),ave(dataset$Age, FUN = function(x) mean(x,na.rm = TRUE)),dataset$Age)
+dataset$Salary = ifelse(is.na(dataset$Salary),ave(dataset$Salary, FUN = function(x) mean(x,na.rm = TRUE)),dataset$Salary)
+
+View(dataset)
 
 # Splitting the dataset into the Training set and Test set
 # install.packages('caTools')
@@ -10,6 +14,9 @@ set.seed(123)
 split = sample.split(dataset$DependentVariable, SplitRatio = 0.8)
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
+
+
+
 
 # Feature Scaling
 # training_set = scale(training_set)
